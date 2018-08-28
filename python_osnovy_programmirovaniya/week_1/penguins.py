8f933e2735031fe753f404801efdc2aaeea06a1e
i.novikov <i.novikov@iconic.vc>
2018-08-28 14:19:57 +0300

    python_osnovy_programmirovaniya

---
+++ python_osnovy_programmirovaniya/week_1/penguins.py
@@ -0,0 +1,18 @@
+__author__ = 'NovikovII'
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+n = int(input())
+
+
+str1 = "   _~_    "
+str2 = "  (o o)   "
+str3 = " /  V  \  "
+str4 = "/(  _  )\\ "
+str5 = "  ^^ ^^   "
+
+print(str1 * n)
+print(str2 * n)
+print(str3 * n)
+print(str4 * n)
+print(str5 * n)
---
+++ python_osnovy_programmirovaniya/week_1/test.py
@@ -0,0 +1,4 @@
+__author__ = 'ivn'
+# -*- coding: utf-8 -*-
+
+

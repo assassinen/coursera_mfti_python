1ecfacd38d341497b0744de28e112c9c4ca0bc66
ivn <ivn@exante.eu>
2018-10-11 22:10:04 +0300

    init

---
+++ any/xlsx_to_csv/xlsx_to_csv.py
@@ -0,0 +1,36 @@
+import csv
+import xlrd
+
+
+def xlsx_reader(path):
+    rb = xlrd.open_workbook(path, formatting_info=False)
+    sheet = rb.sheet_by_index(0)
+    return [sheet.row_values(i) for i in range(sheet.nrows)]
+
+def csv_writer(data, path):
+    """
+    Write data to a CSV file path
+    """
+    with open(path, "w", newline='') as csv_file:
+        writer = csv.writer(csv_file, delimiter=';')
+        for line in data:
+            writer.writerow(line)
+
+
+if __name__ == "__main__":
+    data = ["first_name,last_name,city".split(","),
+            "Tyrese Tyrese,Hirthe,Strackeport".split(","),
+            "Jules,Dicki,Lake Nickolasville".split(","),
+            "Dedric,Medhurst,Stiedemannberg".split(",")
+            ]
+    # 1) записываем в файл output.csv содержимой словаря data
+    out_file = "output.csv"
+    csv_writer(data, out_file)
+
+    # 2) получает данные из файле ddata.xlsx
+    # записывает эти данные в файл
+    in_file = 'ddata.xlsx'
+    out_file = "output_1.csv"
+
+    data = xlsx_reader(in_file)
+    csv_writer(data, out_file)
